8405df1b0a4382fce606b368984886cb096abcfc
Lyubov <letilubov>
2018-12-24 23:55:10 +0300

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rb.AddForce(10, 200, 200);
+    // }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
